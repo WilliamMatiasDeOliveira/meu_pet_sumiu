f35f1171434f59f3f56bb8322befa6642d5e51c3
WilliamMatiasDeOliveira <williammatiasjau@hotmail.com>
2025-10-09 19:58:27 -0300

    create struct folders and views

---
+++ App/Views/home.php
@@ -0,0 +1,11 @@
+<?php
+require_once "Layouts/header.php";
+require_once "Layouts/nav.php";
+?>
+
+
+
+
+<?php
+require_once "Layouts/footer.php";
+?>
--- routes.php
+++ routes.php
@@ -4,5 +4,7 @@ $route = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $route = str_replace("/meu_pet_sumiu", "", $route);
 
 switch($route){
-    
+    case "/":
+
+        break;
 }
